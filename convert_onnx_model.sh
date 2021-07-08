ca40390e1289fb672863894971dfdde1835fa333
Valeriia Koriukina <valeriia.koriukina@xperience.ai>
2021-07-08 18:59:21 +0300

    added nntool_scripts

--- convert_onnx_model.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nntool -s convert_onnx_model.nnscript
-nntool -g nnquant.json -m model.c -T weights
--- convert_tflite_model.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nntool -s convert_tflite_model.nnscript
-nntool -g nnquant.json -m model.c -T weights
