f9b0a48fe7daacb14fb6b3b6e154b788e209b981
Marco Fariselli <marco.fariselli@greenwaves-technologies.com>
2022-08-26 16:19:20 +0200

    Added imagenet test utils

---
+++ models/test_imagenet_tflite.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.append("../../sides/python_utils")
+from imagenet_utils import test_imagenet_tflite, get_synset_dict
+
+model_path = sys.argv[1]
+model_name = os.path.splitext(model_path)[0]
+dataset_path = sys.argv[2]
+print(f"testing model {model_path} on dataset {dataset_path}")
+test_imagenet_tflite(model_path, dataset_path, f"{model_name}_tflite.log", ignore_first_class=True)
